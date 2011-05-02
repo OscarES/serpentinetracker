885794074078ee5e282ac41b93edd9ceccb2bf85
stewart.boogert@gmail.com <stewart.boogert@gmail.com@dc61509f-fb36-9098-7ae9-38b42ea25fa8>
2011-05-02 12:20:09 +0000

    Added EPICS and PyEpics install

--- trunk/install.sh
+++ trunk/install.sh
@@ -40,19 +40,38 @@ tar zxvf pyminuit-1.1.1.tgz
 cd $BASE/Dep/pyminuit
 Python setup.py build
 
-cd $BASE/Dep/base-3.14.12
+cd $BASE/Dep
+tar zxvf baseR3.14.10.tar.gz
+cd $BASE/Dep/base-3.14.10
 export EPICS_HOST_ARCH=`./startup/EpicsHostArch.pl`
+export EPICS_BASE=`pwd`
 make
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$EPICS_BASE/lib/$EPICS_HOST_ARCH
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$EPICS_BASE/lib/$EPICS_HOST_ARCH
+echo export EPICS_BASE=$EPICS_BASE
+echo export EPICS_HOST_ARCH=$EPICS_HOST_ARCH
+echo export PATH=$PATH:$EPICS_BASE/bin/$EPICS_HOST_ARCH/
+
+cd $BASE/Dep/
+tar zxvf pyepics-pyepics-3.1.2-0-g32c56ae.tar
+cd $BASE/Dep/pyepics-pyepics-32c56ae/
+python setup.py build
+
 
 echo " "
 echo "Please add the following lines to your shell configuration (.tcshrc .cshrc. .bashrc .profile)"
 cd $BASE/accformat/programs
 echo export PYTHONPATH=\$PYTHONPATH:$BASE/accformat/programs/build/`for i in \`ls build\`; do echo $i | grep lib; done`
 cd $BASE/Dep/pyminuit
-echo export PYTHONPATH=\$PYTHONPATH:$BASE/Deps/pyminuit/build/`for i in \`ls build\`; do echo $i | grep lib; done`
+echo export PYTHONPATH=\$PYTHONPATH:$BASE/Dep/pyminuit/build/`for i in \`ls build\`; do echo $i | grep lib; done`
+cd $BASE/Dep/pyepics-pyepics-32c56ae
+echo export PYTHONPATH=\$PYTHONPATH:$BASE/Dep/pyepics-pyepics-32c56ae/build/`for i in \`ls build\`; do echo $i | grep lib; done`
 echo export PYTHONPATH=\$PYTHONPATH:$BASE
-echo export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$XERCESCROOT/src/.libs:$MINUITROOT/src/.libs:
+
+echo export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$XERCESCROOT/src/.libs:$MINUITROOT/src/.libs:$EPICS_BASE/lib/$EPICS_HOST_ARCH
 if [ "$STOS" == "Darwin" ]; then
-    echo export DYLD_LIBRARY_PATH=\$DYLD_LIBRARY_PATH:$XERCESCROOT/src/.libs:$MINUITROOT/src/.libs
+    echo export DYLD_LIBRARY_PATH=\$DYLD_LIBRARY_PATH:$XERCESCROOT/src/.libs:$MINUITROOT/src/.libs:$EPICS_BASE/lib/$EPICS_HOST_ARCH
 fi 
+echo export EPICS_BASE=$EPICS_BASE
 echo export EPICS_HOST_ARCH=$EPICS_HOST_ARCH
+echo export PATH=\$PATH:$EPICS_BASE/bin/$EPICS_HOST_ARCH/
